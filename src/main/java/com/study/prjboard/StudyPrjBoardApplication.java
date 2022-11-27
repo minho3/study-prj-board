e3224f18fb6e7135cac2141037366bfab9e3f754
minho <minhoo3@naver.com>
2022-11-27 17:32:22 +0900

    #7 spring 부트 프로젝트 시작
    스프링 부트 프로젝트 시작
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/study/prjboard/StudyPrjBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.prjboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPrjBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPrjBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/prjboard/StudyPrjBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.prjboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPrjBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
